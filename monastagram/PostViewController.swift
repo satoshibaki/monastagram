adb9bdf8d48693f865e53462e0c8db69534587ab
satoshibaki <snm-792@mbox.re>
2021-02-21 11:20:02 +0900

    新規登録

---
+++ monastagram/PostViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PostViewController.swift
+//  monastagram
+//
+//  Created by 中村智史 on 2021/02/21.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
