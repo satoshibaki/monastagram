702e1171c26d7b5ad3271aa5be9701a776aefec1
satoshibaki <snm-792@mbox.re>
2021-02-21 09:38:26 +0900

    Initial Commit

---
+++ monastagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  monastagram
+//
+//  Created by 中村智史 on 2021/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
